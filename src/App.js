e024dd3bed4253a3cd823f0f5f2332ff28ca5175
nuumrea <kevin.parayre@gmail.com>
2022-03-15 12:40:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import YtEmbed from "./components/ytEmbed/index";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <span>
+          Lost Ark new class : Artist <br />
+          Release Soon
+          <p>Source : Trust me bro</p>
+        </span>
+      </div>
+      <YtEmbed embedId="wl0q0xi10Ng" />
     </div>
   );
-}
+};
 
 export default App;
