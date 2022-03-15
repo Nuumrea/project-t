adf57b1df0f44d4ba74c999acc2b7f9bc287818a
nuumrea <kevin.parayre@gmail.com>
2022-03-15 13:17:51 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css";
 import YtEmbed from "./components/ytEmbed/index";
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
 
 const App = () => {
   return (
-    <div className="app">
-      <div>
-        <span>
-          Lost Ark new class : Artist <br />
-          Release Soon
-          <p>Source : Trust me bro</p>
-        </span>
+    <>
+      <div className="app">
+        <div>
+          <span>
+            Lost Ark new class : Artist <br />
+            Release Soon
+            <p>Source : Trust me bro</p>
+          </span>
+        </div>
+        <YtEmbed embedId="wl0q0xi10Ng" />
       </div>
-      <YtEmbed embedId="wl0q0xi10Ng" />
-    </div>
+      <LinkPreview
+        url="https://www.youtube.com/watch?v=wl0q0xi10Ng"
+        width="400px"
+      />
+      ;
+    </>
   );
 };
 
